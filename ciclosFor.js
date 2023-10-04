ab7fbb54e440ac832d08730ec9fd38f1fd78fa95
MarioMozo <mario.psy@live.com>
2023-10-04 12:14:05 -0600

    Ciclo while

---
+++ ciclosFor.js
@@ -0,0 +1,12 @@
+var arregloNombres = ["Frida", "Tomas", "Wilson"];
+
+function saludarEstudiante(arregloNombres){
+    console.log('Hola: ', ${arregloNombres});
+
+}
+
+
+for(var i = 0; i < arregloNombres.length; i++){
+    saludarEstudiante(arregloNombres[i])
+}
+//recorre el arreglo y llama a la funcion en cada iteracion.
