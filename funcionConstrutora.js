3fdcb0d73e9961f6a74351e09556d14ea3b7d13b
MarioMozo <mario.psy@live.com>
2023-10-04 12:31:54 -0600

    Funcion constructora

---
+++ funcionConstrutora.js
@@ -0,0 +1,11 @@
+function auto(marca, modelo, anio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+
+}
+
+//hacer una instancia de la funcion constructora
+
+//Constructor
+var autoNuevo = new auto("Tesla", "ModelX", 2020);
