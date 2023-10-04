555a69e6df1ed23478f73df2a3010e711920db4f
MarioMozo <mario.psy@live.com>
2023-10-04 10:34:44 -0600

    Creacion del repositorio primeros apuntos

---
+++ valores.js
@@ -0,0 +1,14 @@
+//Tipo entero
+13
+//Tipo booleanos verdadero/falso
+true
+false
+//Tipo String
+"Mario Ivan"
+
+//Variables
+
+//String
+var nombre = "Mario";
+//Array
+var elementos = [2,3,4,5,5];
